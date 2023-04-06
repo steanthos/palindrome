feffcd5c78492759c591439d919e2a1f5c41f086
Stefano Antonini <steanthos@gmail.com>
2023-04-06 11:58:54 +0200

    Initialize repository

---
+++ index.js
@@ -0,0 +1,24 @@
+// Adds 'reverse' to all strings.
+String.prototype.reverse = function() {
+	return Array.from(this).reverse().join("");
+}
+
+//Defines a Phrase object
+function Phrase(content) {
+	this.content = content;
+
+  //Returns content processed for a palindrome
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+// Returns true if the phrase is a palindrome
+	this.palindrome = function palindrome() {
+		return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+
+
+
+	
